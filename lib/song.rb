5648694741a6a2f3e152095398dee2be3f8daefe
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-06 04:40:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,5 +29,16 @@ class Song
     @@artists.uniq
   end
   
+   def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   
 end
